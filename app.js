c3caaca778455d8520ff8498c8ee5d251a945752
mogit2020 <mohibullah.kamal@gmail.com>
2018-12-26 12:06:44 +0100

    Fully functional .emit and .on

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const EventEmitter = require('events');  // 'events' is a class with lots of other methods therefore we initialize it with capital letter and camel case
 
 const Logger = require('./logger');
-const logger = new Logger();   // create new instance
+const logger = new Logger();  // create new instance
 
 // Register a listener
-logger.on('messageLogged')
+logger.on('messageLogged', (arg) => {
+    console.log('Listener called --> ', arg);
+});
+
+logger.log('message');
+
--- logger.js
+++ logger.js
@@ -1,7 +1,13 @@
 const EventEmitter = require('events');  // 'events' is a class with lots of other methods therefore we initialize it with capital letter and camel case
 
 class Logger extends EventEmitter {
+    log(message) {
+        // Send an HTTP request
+        console.log(message);
 
+        // Raise an event - 'emit' is an important method of 'events' Class of nodeJS
+        this.emit('messageLogged', { id: 1, url: 'http://' });
+    }
 }
 
 module.exports = Logger;
