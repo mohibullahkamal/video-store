f1a499f80345eb64f5ef84a1d47d65bb9c372d90
mogit2020 <mohibullah.kamal@gmail.com>
2018-12-26 14:00:52 +0100

    Connected to Port 3000

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
-const EventEmitter = require('events');  // 'events' is a class with lots of other methods therefore we initialize it with capital letter and camel case
+//**************** HTTP MODULE ****************
+//**********************************************
+const http = require('http');
 
-const Logger = require('./logger');
-const logger = new Logger();  // create new instance
+const server = http.createServer();
 
-// Register a listener
-logger.on('messageLogged', (arg) => {
-    console.log('Listener called --> ', arg);
+var visits = 0;
+// create localhost connection... 
+server.on('connection', (Socket) => {
+    console.log('New connection... visits=' + visits);
+    visits++;
 });
+server.listen(3000);
 
-logger.log('message');
+
+
+
+// //**************** EVENT MODULE ****************
+// //**********************************************
+// const EventEmitter = require('events');  // 'events' is a class with lots of other methods therefore we initialize it with capital letter and camel case
+
+// const Logger = require('./logger');
+// const logger = new Logger();  // create new instance
+
+// // Register a listener
+// logger.on('messageLogged', (arg) => {
+//     console.log('Listener called --> ', arg);
+// });
+
+// logger.log('message');
 
--- logger.js
+++ logger.js
@@ -1,13 +1,26 @@
-const EventEmitter = require('events');  // 'events' is a class with lots of other methods therefore we initialize it with capital letter and camel case
+//**************** HTTP MODULE ****************
+//**********************************************
 
-class Logger extends EventEmitter {
-    log(message) {
-        // Send an HTTP request
-        console.log(message);
 
-        // Raise an event - 'emit' is an important method of 'events' Class of nodeJS
-        this.emit('messageLogged', { id: 1, url: 'http://' });
-    }
-}
 
-module.exports = Logger;
+
+
+
+
+// //**************** EVENT MODULE ****************
+// //**********************************************
+// const EventEmitter = require('events');  // 'events' is a class with lots of other methods therefore we initialize it with capital letter and camel case
+
+// var url = 'http://thisIsJustTest.io/log';
+
+// class Logger extends EventEmitter {
+//     log(message) {
+//         // Send an HTTP request
+//         console.log(message);
+
+//         // Raise an event - 'emit' is an important method of 'events' Class of nodeJS
+//         this.emit('messageLogged', { id: 1, url });
+//     }
+// }
+
+// module.exports = Logger;
