4f4ca4b011307c520093f445fda08f5eef48a5af
mogit2020 <mohibullah.kamal@gmail.com>
2018-12-27 15:12:55 +0100

    express file (res, res) set

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,2 +1,20 @@
 const express = require('express');
-express();
+const app = express();
+
+let homeCount = 1;
+app.get('/', (req, res) => {   // This is how we define a route... we specify the url/path and then a callback function also called a route handler... 
+    res.send('Hello From Home...');
+    console.log('Home...count = ' + homeCount);
+    return homeCount++;
+});
+
+let apiCount = 1;
+app.get('/api/courses', (req, res) => {
+    res.send('Hello from api/courses...');
+    console.log('Api... count = ' + apiCount);
+    return apiCount++;
+});
+
+app.listen(3000, () => {   // we set a listener... 
+    console.log('Listening on PORT 3000... ');
+});
