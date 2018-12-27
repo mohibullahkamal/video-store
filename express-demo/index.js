0729c2aaf8c83c04dbccaa422c74e5c8326e3cbf
mogit2020 <mohibullah.kamal@gmail.com>
2018-12-27 17:59:51 +0100

    Add more req,res Paths

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 
+// our data set array... 
+const courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' },
+    { id: 4, name: 'course4' },
+    { id: 5, name: 'course5' },
+];
+
+
+
 let homeCount = 1;
 app.get('/', (req, res) => {   // This is how we define a route... we specify the url/path and then a callback function also called a route handler... 
     res.send('Hello From Home...');
@@ -15,6 +26,40 @@ app.get('/api/courses', (req, res) => {
     return apiCount++;
 });
 
-app.listen(3000, () => {   // we set a listener... 
-    console.log('Listening on PORT 3000... ');
+// How to get "/api/courses/1"; ":" is used to set a parameter... in this case id
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);   // you basically can read the url id... just imagine what power it gives you... 
+});
+
+// now look at this... we can basically provide all info in url... cool... /api/posts/2018/February...
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.params);
+});
+
+// but how to read "Query String Parameters"... here is how... same as above but minute change... 
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.query);   // Now our app can read query from url... eg: http://localhost:3000/api/posts/3000/March?sortBy=name  ... in this case {sortBy: "name"}
+});
+
+// Now after setting the courses array above... lets get all courses...
+app.get('/api/courses', (req, res) => {
+    res.send(courses);
+});
+
+// Lets now set id for courses to get... a bit complicated though... Naaa its easy... 
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));       // set the following to 'const course'; and also parse the req info...     // courses.find is a method available to every JS array, it takes an argument... in order for the comparison to properly we are converting; as in parsing it...  
+    if (!course) res.status(404).send('The course with the given ID was not found... ');    // check for error... blablabla.send .... always have a .send
+    res.send(course);    //else just send(course)
+});
+
+
+
+
+
+
+// PORT - it is unlikely that our hosting port will be 3000... therefore to set it dynamically we use this process.env method... 
+const port = process.env.PORT || 3000;
+app.listen(port, () => {   // we set a listener... 
+    console.log(`Listening on PORT ${port}... `);
 });
