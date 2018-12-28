64e1f47a7d872a2ab140304f77a42cb199a6acd3
mogit2020 <mohibullah.kamal@gmail.com>
2018-12-28 11:58:31 +0100

    Set post request

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());   // allows for Json parsing... not set by default by express... we need it to parse the request below... here-->  "name: req.body.name"
+
 // our data set array... 
 const courses = [
     { id: 1, name: 'course1' },
@@ -11,49 +13,56 @@ const courses = [
 ];
 
 
+// let homeCount = 1;
+// app.get('/', (req, res) => {   // This is how we define a route... we specify the url/path and then a callback function also called a route handler... 
+//     res.send('Hello From Home...');
+//     console.log('Home...count = ' + homeCount);
+//     return homeCount++;
+// });
 
-let homeCount = 1;
-app.get('/', (req, res) => {   // This is how we define a route... we specify the url/path and then a callback function also called a route handler... 
-    res.send('Hello From Home...');
-    console.log('Home...count = ' + homeCount);
-    return homeCount++;
+// let apiCount = 1;
+// app.get('/api/courses', (req, res) => {
+//     res.send('Hello from api/courses...');
+//     console.log('Api... count = ' + apiCount);
+//     return apiCount++;
+// });
+
+// // How to get "/api/courses/1"; ":" is used to set a parameter... in this case id
+// app.get('/api/courses/:id', (req, res) => {
+//     res.send(req.params.id);   // you basically can read the url id... just imagine what power it gives you... 
+// });
+
+// // now look at this... we can basically provide all info in url... cool... /api/posts/2018/February...
+// app.get('/api/posts/:year/:month', (req, res) => {
+//     res.send(req.params);
+// });
+
+// // but how to read "Query String Parameters"... here is how... same as above but minute change... 
+// app.get('/api/posts/:year/:month', (req, res) => {
+//     res.send(req.query);   // Now our app can read query from url... eg: http://localhost:3000/api/posts/3000/March?sortBy=name  ... in this case {sortBy: "name"}
+// });
+
+// // Now after setting the courses array above... lets get all courses...
+// app.get('/api/courses', (req, res) => {
+//     res.send(courses);
+// });
+
+// // Lets now set id for courses to get... a bit complicated though... Naaa its easy... 
+// app.get('/api/courses/:id', (req, res) => {
+//     const course = courses.find(c => c.id === parseInt(req.params.id));   // look above what params.id does... set the following to 'const course'; and also parse the req info // courses.find is a method available to every JS array, it takes an argument... in order for the comparison to properly we are converting; as in parsing it...  
+//     if (!course) res.status(404).send('The course with the given ID was not found... ');    // check for error... blablabla.send .... always have a .send
+//     res.send(course);    //else just send(course)
+// });
+
+// This is Post request... I had to set app.get(express.json) .... above.. 
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,   // we add 1 simple
+        name: req.body.name   // we use -> app.use(express.json()) parsing above... 
+    };
+    courses.push(course);   // we push the newly added course object... 
+    res.send(course);   // convention that db notifies to which id was added... 
 });
-
-let apiCount = 1;
-app.get('/api/courses', (req, res) => {
-    res.send('Hello from api/courses...');
-    console.log('Api... count = ' + apiCount);
-    return apiCount++;
-});
-
-// How to get "/api/courses/1"; ":" is used to set a parameter... in this case id
-app.get('/api/courses/:id', (req, res) => {
-    res.send(req.params.id);   // you basically can read the url id... just imagine what power it gives you... 
-});
-
-// now look at this... we can basically provide all info in url... cool... /api/posts/2018/February...
-app.get('/api/posts/:year/:month', (req, res) => {
-    res.send(req.params);
-});
-
-// but how to read "Query String Parameters"... here is how... same as above but minute change... 
-app.get('/api/posts/:year/:month', (req, res) => {
-    res.send(req.query);   // Now our app can read query from url... eg: http://localhost:3000/api/posts/3000/March?sortBy=name  ... in this case {sortBy: "name"}
-});
-
-// Now after setting the courses array above... lets get all courses...
-app.get('/api/courses', (req, res) => {
-    res.send(courses);
-});
-
-// Lets now set id for courses to get... a bit complicated though... Naaa its easy... 
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));       // set the following to 'const course'; and also parse the req info...     // courses.find is a method available to every JS array, it takes an argument... in order for the comparison to properly we are converting; as in parsing it...  
-    if (!course) res.status(404).send('The course with the given ID was not found... ');    // check for error... blablabla.send .... always have a .send
-    res.send(course);    //else just send(course)
-});
-
-
 
 
 
