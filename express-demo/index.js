e12cdde9aee19574b37c65d6d97a9fb8544caf09
mogit2020 <mohibullah.kamal@gmail.com>
2018-12-29 09:07:23 +0100

    Create Schema for course Object

--- express-demo/index.js
+++ express-demo/index.js
@@ -13,7 +13,7 @@ const courses = [
     { id: 5, name: 'course5' },
 ];
 
-
+//******************************* */
 // let homeCount = 1;
 // app.get('/', (req, res) => {   // This is how we define a route... we specify the url/path and then a callback function also called a route handler... 
 //     res.send('Hello From Home...');
@@ -33,6 +33,7 @@ const courses = [
 //     res.send(req.params.id);   // you basically can read the url id... just imagine what power it gives you... 
 // });
 
+//******************************* */
 // // now look at this... we can basically provide all info in url... cool... /api/posts/2018/February...
 // app.get('/api/posts/:year/:month', (req, res) => {
 //     res.send(req.params);
@@ -48,6 +49,7 @@ const courses = [
 //     res.send(courses);
 // });
 
+//******************************* */
 // // Lets now set id for courses to get... a bit complicated though... Naaa its easy... 
 // app.get('/api/courses/:id', (req, res) => {
 //     const course = courses.find(c => c.id === parseInt(req.params.id));   // look above what params.id does... set the following to 'const course'; and also parse the req info // courses.find is a method available to every JS array, it takes an argument... in order for the comparison to properly we are converting; as in parsing it...  
@@ -65,14 +67,15 @@ const courses = [
 //     res.send(course);   // convention that db notifies to which id was added... 
 // });
 
+//******************************* */
 // we set input validator... copy of above... plus the input validator... 
 app.post('/api/courses', (req, res) => {
-    // we define schema of our object...
-    const schema = {
-        name: Joi.string().min(3).required()
+    const schema = {    // ***We define JOI Schema for our object... a Schema defines the shape of our Object -> what type, props, etc we will use... 
+        name: Joi.string().min(3).required()   // We are telling JOI that this a string, minimum 3 chars, and required means must be entered by user... 
     }
 
-    Joi.validate(req.body, schema);
+    const result = Joi.validate(req.body, schema);   // We give JOI req.body as well as the schema we declared... 
+    console.log(result);
 
     if (!req.body.name || req.body.name.length < 3) {   // never trust data sent from client... 
         // 400 Bad Request... 
@@ -86,6 +89,7 @@ app.post('/api/courses', (req, res) => {
     courses.push(course);   // we push the newly added course object... 
     res.send(course);   // convention that db notifies to which id was added... 
 });
+
 
 
 
