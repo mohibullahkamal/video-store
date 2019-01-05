351e550e9acd0088d447fbab9491c55c822f65b9
mogit2020 <mohibullah.kamal@gmail.com>
2019-01-05 18:27:28 +0100

    settup new router home.js

---
+++ express-demo/routes/home.js
@@ -0,0 +1,10 @@
+const express = require('express');
+// const app = express();   // this don't work when you sepate the routes in a separate module... so here we need to use a router... 
+const router = express.Router();   // see explanation above as to why we are using a router... when you use express from different module then "express()" doesnot work... WE HAVE TO USE ROUTER... 
+
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'My Express App', message: 'Hello, this is my App...' });
+});
+
+module.exports = router; 
