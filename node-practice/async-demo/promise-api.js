a6eeb600c8e1b8402a0bff2eaa3b885b3efe5798
mogit2020 <mohibullah.kamal@gmail.com>
2019-01-25 10:28:40 +0100

    add promise-api.js

--- node-practice/async-demo/promise-api.js
+++ node-practice/async-demo/promise-api.js
@@ -1,8 +1,15 @@
 
 // this 'promise-api.js' file is a playground file; which contains promise file; which is already resolved and is useful for writing unit tests
 // so basically you want to simulate a senario where the promise completes successfully
-const p1 = new Promise((resolve) => {
-    setTimeout(() => {
 
-    }, 2000);
-});
+// const p = Promise.resolve({ id: 1 });   // this is for resolve.... 
+// p.then(result => console.log(result));
+
+const p = Promise.reject(new Error('reason for rejection...'));   // this is for reject.... 
+p.catch(error => console.log(error));
+
+// const p1 = new Promise((resolve) => {
+//     setTimeout(() => {
+
+//     }, 2000);
+// });
