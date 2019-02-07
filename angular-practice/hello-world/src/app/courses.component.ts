8f5c3f62f40ab8859aa8ffff9aac89e2fa734467
mogit2020 <mohibullah.kamal@gmail.com>
2019-02-07 10:26:42 +0100

    add h2 component to courses.component

--- angular-practice/hello-world/src/app/courses.component.ts
+++ angular-practice/hello-world/src/app/courses.component.ts
@@ -1,3 +1,9 @@
+import { Component } from '@angular/core';   //we are importing 'component' decorator from angularCore
+
+@Component({
+    selector: 'courses',   // we write selectors this way.... <courses> "courses"; <div class="courses"> ".courses"; <div id="courses"> "#courses"
+    template: '<h2>Courses</h2>'
+})   //this is the decorator function... 
 export class CoursesComponent {
 
 }
