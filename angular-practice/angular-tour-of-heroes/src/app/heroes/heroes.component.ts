a9821173ef4db6248ddddca8267d0d8dcba9d506
mogit2020 <mohibullah.kamal@gmail.com>
2019-02-28 14:17:07 +0100

    Innitialize hero property to be of type Hero

--- angular-practice/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-practice/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Hero } from '../hero';   // import class Hero
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 
   constructor() { }
 
