162c84a350cc33e602b14b44941e734a6aa974a2
mogit2020 <mohibullah.kamal@gmail.com>
2019-03-05 20:47:55 +0100

    add *ngIf

--- angular-practice/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-practice/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -7,9 +7,15 @@ import { HEROES } from '../mock-heroes';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
+
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() { }
 
