bdd33aa83c15f9c96b7ae7d14e3b17c3b6d5fd14
mogit2020 <mohibullah.kamal@gmail.com>
2019-03-05 20:19:26 +0100

    add heroes component style

--- angular-practice/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-practice/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
-
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
