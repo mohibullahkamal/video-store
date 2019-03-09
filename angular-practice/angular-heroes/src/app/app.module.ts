8b2574b82edc722e45b9103ca8964ce00012aacd
mogit2020 <mohibullah.kamal@gmail.com>
2019-03-09 11:37:30 +0100

    delete vehicles component

--- angular-practice/angular-heroes/src/app/app.module.ts
+++ angular-practice/angular-heroes/src/app/app.module.ts
@@ -5,13 +5,11 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 import { FormsModule } from '@angular/forms';
-import { VehiclesComponent } from './vehicles/vehicles.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    VehiclesComponent
   ],
   imports: [
     BrowserModule,
