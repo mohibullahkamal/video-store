a2bb98bc5924e2340dfc729a42d1992e6c5eaa5e
mogit2020 <mohibullah.kamal@gmail.com>
2019-03-10 22:03:39 +0100

    add new hero files

--- angular-practice/angular-heroes/src/app/heroes/heroes.component.ts
+++ angular-practice/angular-heroes/src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+
+  hero = 'Windstorm...';
 
   constructor() { }
 
