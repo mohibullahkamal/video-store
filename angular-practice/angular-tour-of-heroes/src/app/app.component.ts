3903d96f578fa295ba52467792b3c317adb443ee
mogit2020 <mohibullah.kamal@gmail.com>
2019-03-22 22:08:53 +0100

    update app.component.css

--- angular-practice/angular-tour-of-heroes/src/app/app.component.ts
+++ angular-practice/angular-tour-of-heroes/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { title } from '../'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  title = 'Tour of Heroes';
 }
