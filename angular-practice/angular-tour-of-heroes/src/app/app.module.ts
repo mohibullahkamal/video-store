783040a31ebd3dcaaac3413de0166929e355c9f9
mogit2020 <mohibullah.kamal@gmail.com>
2019-03-24 21:58:23 +0100

    add car component

--- angular-practice/angular-tour-of-heroes/src/app/app.module.ts
+++ angular-practice/angular-tour-of-heroes/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { CarsComponent } from './cars/cars.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    CarsComponent
   ],
   imports: [
     BrowserModule
