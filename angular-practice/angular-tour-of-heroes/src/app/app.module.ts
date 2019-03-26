578b9ef19f05a060de5372b70062b02e2e08a44e
mogit2020 <mohibullah.kamal@gmail.com>
2019-03-26 13:50:56 +0100

    add drivers component

--- angular-practice/angular-tour-of-heroes/src/app/app.module.ts
+++ angular-practice/angular-tour-of-heroes/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { CarsComponent } from './cars/cars.component';
 import { AccessoriesComponent } from './accessories/accessories.component';
+import { DriversComponent } from './drivers/drivers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     CarsComponent,
-    AccessoriesComponent
+    AccessoriesComponent,
+    DriversComponent
   ],
   imports: [
     BrowserModule
