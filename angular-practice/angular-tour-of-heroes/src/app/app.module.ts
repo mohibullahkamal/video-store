e87026c3c30c77dfa361b3ef8abd1a1333b5cf68
mogit2020 <mohibullah.kamal@gmail.com>
2019-03-29 18:35:15 +0100

    delete components

--- angular-practice/angular-tour-of-heroes/src/app/app.module.ts
+++ angular-practice/angular-tour-of-heroes/src/app/app.module.ts
@@ -3,19 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { CarsComponent } from './cars/cars.component';
-import { AccessoriesComponent } from './accessories/accessories.component';
-import { DriversComponent } from './drivers/drivers.component';
-import { RoadTypeComponent } from './road-type/road-type.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    CarsComponent,
-    AccessoriesComponent,
-    DriversComponent,
-    RoadTypeComponent
   ],
   imports: [
     BrowserModule
