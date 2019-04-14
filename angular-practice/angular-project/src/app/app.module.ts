8d05ec5c5b2d9cdf8b809c81e232f8f7e1a4d080
mogit2020 <mohibullah.kamal@gmail.com>
2019-04-14 02:23:49 +0200

    add heroList component

--- angular-practice/angular-project/src/app/app.module.ts
+++ angular-practice/angular-project/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DataComponent } from './data/data.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    DataComponent
+    DataComponent,
+    HeroListComponent
   ],
   imports: [
     BrowserModule,
