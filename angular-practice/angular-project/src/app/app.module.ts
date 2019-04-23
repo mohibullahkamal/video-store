a87a043412287b8e029fa877fe0c5e5f511d7111
mogit2020 <mohibullah.kamal@gmail.com>
2019-04-23 14:27:30 +0200

    fix code bugs

--- angular-practice/angular-project/src/app/app.module.ts
+++ angular-practice/angular-project/src/app/app.module.ts
@@ -4,14 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { UsersComponent } from './users/users.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
-    UsersComponent
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
