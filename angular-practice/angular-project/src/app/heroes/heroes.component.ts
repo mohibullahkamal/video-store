498e6dd9929d48ceda81fba3901725b913d4ec5c
mogit2020 <mohibullah.kamal@gmail.com>
2019-04-24 16:44:05 +0200

    delete previous trial codes

--- angular-practice/angular-project/src/app/heroes/heroes.component.ts
+++ angular-practice/angular-project/src/app/heroes/heroes.component.ts
@@ -11,11 +11,6 @@ export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
   selectedHero: Hero;
-  
-  // abc: Hero = {
-  //   id: 1,
-  //   name: 'Windstorm'
-  // };
 
   constructor() { }
   ngOnInit() { }
