b9beb3af3909f047113c8a9a258ffeb1996bab3a
mogit2020 <mohibullah.kamal@gmail.com>
2019-05-02 08:03:52 +0200

    update user component

--- angular-practice/angular-project/src/app/app.module.ts
+++ angular-practice/angular-project/src/app/app.module.ts
@@ -4,14 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { UserComponent } from './user/user.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
-    UserComponent
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
