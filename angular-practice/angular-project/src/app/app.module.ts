0e845d64583c94d14439b2a01df905da9ffffe13
mogit2020 <mohibullah.kamal@gmail.com>
2019-05-08 07:16:28 +0200

    generate user component

--- angular-practice/angular-project/src/app/app.module.ts
+++ angular-practice/angular-project/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { UserComponent } from './user/user.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
