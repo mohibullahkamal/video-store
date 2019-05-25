a5d5e42d2ca25c41787116e7edcbbae0a9fbf5db
mogit2020 <mohibullah.kamal@gmail.com>
2019-05-25 10:35:52 +0200

    update project

--- angular-practice/angular-project/src/app/app.module.ts
+++ angular-practice/angular-project/src/app/app.module.ts
@@ -6,14 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroNameComponent } from './hero-name/hero-name.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent,
-    HeroNameComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
