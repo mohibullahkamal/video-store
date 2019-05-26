beb4c0d2e86518a49f785d334452b670c762764f
mogit2020 <mohibullah.kamal@gmail.com>
2019-05-26 08:51:22 +0200

    add userid component

--- angular-practice/angular-project/src/app/app.module.ts
+++ angular-practice/angular-project/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { UseridComponent } from './userid/userid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    UseridComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-practice/angular-project/src/app/userid/userid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-userid',
+  templateUrl: './userid.component.html',
+  styleUrls: ['./userid.component.css']
+})
+export class UseridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
