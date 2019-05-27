510c9c2527137c2bfba65ca62b7f5f25bca85d63
mogit2020 <mohibullah.kamal@gmail.com>
2019-05-27 10:35:53 +0200

    delete user-detail componene

--- angular-practice/angular-project/src/app/app.module.ts
+++ angular-practice/angular-project/src/app/app.module.ts
@@ -5,15 +5,11 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { UseridComponent } from './userid/userid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    UseridComponent
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
