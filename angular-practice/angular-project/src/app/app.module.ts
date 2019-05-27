7f8bd449a3f5ada6c87581d0636fbf00f5469e74
mogit2020 <mohibullah.kamal@gmail.com>
2019-05-27 11:13:31 +0200

    add hero-name component

--- angular-practice/angular-project/src/app/app.module.ts
+++ angular-practice/angular-project/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroNameComponent } from './hero-name/hero-name.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroNameComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-practice/angular-project/src/app/hero-name/hero-name.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-name',
+  templateUrl: './hero-name.component.html',
+  styleUrls: ['./hero-name.component.css']
+})
+export class HeroNameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
