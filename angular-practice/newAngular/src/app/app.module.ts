03e4bf009b516b387f3873ec31f94869e9db9b1c
mogit2020 <mohibullah.kamal@gmail.com>
2019-05-30 08:33:55 +0200

    add new Hero class

--- angular-practice/newAngular/src/app/app.module.ts
+++ angular-practice/newAngular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
