74cfa8a9bb5c90983edf8615acc66a33945c0091
mogit2020 <mohibullah.kamal@gmail.com>
2019-06-03 14:27:54 +0200

    he

--- angular-practice/newAngular/src/app/app.module.ts
+++ angular-practice/newAngular/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { CarComponent } from './car/car.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
+    CarComponent,
   ],
   imports: [
     BrowserModule,
