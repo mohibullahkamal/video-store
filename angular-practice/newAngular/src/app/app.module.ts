4b35bf58d399d3be8ecb7fb8335c86700bd5f9f4
mogit2020 <mohibullah.kamal@gmail.com>
2019-06-06 05:13:49 +0200

    delete component element

--- angular-practice/newAngular/src/app/app.module.ts
+++ angular-practice/newAngular/src/app/app.module.ts
@@ -5,13 +5,11 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { ElementComponent } from './element/element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    ElementComponent,
   ],
   imports: [
     BrowserModule,
