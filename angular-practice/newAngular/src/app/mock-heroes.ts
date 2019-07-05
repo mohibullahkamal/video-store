63280296a55ecd216e28f494b0c79ad9b46c39e1
Mohibullah Kamal <mohibullah.kamal@gmail.com>
2019-07-05 08:52:39 +0200

    Delete mock-heroes.ts

--- angular-practice/newAngular/src/app/mock-heroes.ts
+++
@@ -1,8 +0,0 @@
-import { Hero } from './hero';
-
-export const HEROES :Hero[] = [
-    { id: 11, name: 'Dr Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 12, name: 'Narco' },
-    { id: 12, name: 'Narco' },
-];
