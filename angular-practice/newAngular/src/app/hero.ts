d1fd42782dcd94334bfeeca0c5dd071b5da147e9
Mohibullah Kamal <mohibullah.kamal@gmail.com>
2019-07-18 08:27:19 +0200

    Update hero.ts

--- angular-practice/newAngular/src/app/hero.ts
+++ angular-practice/newAngular/src/app/hero.ts
@@ -1,9 +1,12 @@
 export class Hero {
-    id: number;
-    name: string;
-    email: string; 
-    phone: number;
-    street: number;
-    city: string;
-    land: string;
+    id: number,
+    name: string,
+    email: string, 
+    phone: number, 
+    street: number, 
+    city: string, 
+    land: string, 
+    school: string, 
+    course: string, 
+    beginYear: string,
 }
