4593777956a70a1fb2925c6638287703cfe5d2dd
Mohibullah Kamal <mohibullah.kamal@gmail.com>
2019-08-06 09:59:22 +0200

    Update mock-heroes.ts

--- angular-practice/newAngular/src/app/mock-heroes.ts
+++ angular-practice/newAngular/src/app/mock-heroes.ts
@@ -4,4 +4,7 @@ export const HEROES:Hero[] = [
     {id: 11, name: 'Albert'},
     {id: 12, name: 'Andy'},
     {id: 13, name: 'Einstein'},
+    {id: 14, name: 'Wes'},
+    {id: 15, name: 'Andreau'},
+    {id: 16, name: 'James'},
 ];
